5d9105a274fcbf096e33827febc137ba5fa0a88a
mahmoud-ramadn <mr9026573@gmail.com>
2025-02-20 23:45:50 +0200

    Fix:Contact-us-Section

--- components/Principles.vue
+++ components/Principles.vue
@@ -14,18 +14,18 @@ const principles = [
     <ui-title title="المبادئ" />
     <div class="text-center       grid  lg:grid-cols-4 grid-cols-1   gap-y-10  mt-8   mx-0   p-0 h-full    "
     >
-    <div class=" lg:col-span-2   col-span-1    relative   flex justify-center items-center     pr-[25%]  py-8   lg:h-[475px] md:h-[400px] h-[380px] ">
+    <div class="  col-span-2   relative   flex justify-center items-center     pr-[25%]  py-8   lg:h-[475px] md:h-[400px] h-[380px] ">
       <div
-        class="bg-[url('@/assets/images/market.webp')]   bg-no-repeat bg-cover  absolute right-0  z-10   top-0   lg:w-[65%]   md:w-[70%]    w-[90%]   bg-center  py-16  h-full    "
+        class="bg-[url('@/assets/images/market.webp')]   bg-no-repeat bg-cover  absolute right-0  z-10   top-0     w-[61%]   bg-center  py-16  h-full    "
       >
     </div>
     <img
        src="@/assets/images/principles.webp"
        alt="Principles Illustration"
-       class="border-r-[10px] border-white  w-full  border-b-[9px] rounded-2xl  object-cover aspect-[3/4]  z-10    h-full "
+       class="border-r-[10px] border-white  w-[90%]  border-b-[9px] rounded-2xl  object-cover z-10     h-[81%] "
      />
     </div>
-       <div class="    lg:col-span-2 col-span-1 lg:flex-row flex-col     gap-y-6 justify-center     w-full flex items-center  h-full  px-4 ">
+       <div class="    lg:col-span-2  col-span-1 lg:flex-row flex-col     gap-y-6 justify-center     w-full flex items-center  h-full  px-4 ">
         <div class="  space-y-7   lg:max-w-[273px] w-full ">
           <princple-item
             v-for="(principle, index) in principles.slice(0, 3)"
