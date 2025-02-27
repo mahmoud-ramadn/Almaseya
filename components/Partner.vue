cb854ce2a017bfe51163c75918e3b5db9b456f3e
mahmoud-ramadn <mr9026573@gmail.com>
2025-02-27 19:32:01 +0200

    remove:unsue tage

--- components/Partner.vue
+++ components/Partner.vue
@@ -13,7 +13,7 @@
           <img
             :src="item.image"
             :alt="item.id"
-            class="object-cover size-10 aspect-square"
+            class="object-cover size-10 aspect-square   rounded-lg"
             loading="lazy"
           />
           <p>{{ item.title }}</p>
@@ -29,65 +29,124 @@ import { cn } from "@/lib/utils";
 const items = [
   {
     id: 1,
-    title: "ميرو",
-    image: new URL("@/assets/images/partners/1.webp", import.meta.url).href,
+    title: "تسلا",
+    image:"https://static.vecteezy.com/system/resources/previews/020/336/044/non_2x/tesla-logo-tesla-icon-transparent-png-free-vector.jpg",
   },
   {
     id: 2,
     title: "مايكروسوفت",
-    image: new URL("@/assets/images/partners/2.webp", import.meta.url).href,
+    image:"https://w7.pngwing.com/pngs/719/781/png-transparent-windows-logo-microsoft-windows-scalable-graphics-logo-computer-file-microsoft-logo-icon-angle-text-rectangle.png",
   },
+
   {
     id: 3,
-    title: "تيسلا",
-    image: new URL("@/assets/images/partners/3.webp", import.meta.url).href,
+    title: "ادوبي xd",
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUZK5gi1clbPagXVx8PLkTK8qhlBCjve9oRw&s",
   },
   {
     id: 4,
-    title: "أوتو",
-    image: new URL("@/assets/images/partners/4.webp", import.meta.url).href,
+   title: "مايكروسوفت",
+    image:"https://w7.pngwing.com/pngs/719/781/png-transparent-windows-logo-microsoft-windows-scalable-graphics-logo-computer-file-microsoft-logo-icon-angle-text-rectangle.png",
+  },
+  {
+    id: 4,
+    title: "ادوبي xd",
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUZK5gi1clbPagXVx8PLkTK8qhlBCjve9oRw&s",
   },
   {
     id: 5,
-    title: "ادوبي xd",
-    image: new URL("@/assets/images/partners/5.webp", import.meta.url).href,
+    title: "تسلا",
+    image:"https://static.vecteezy.com/system/resources/previews/020/336/044/non_2x/tesla-logo-tesla-icon-transparent-png-free-vector.jpg",
   },
   {
     id: 6,
-    title: "ميرو",
-    image: new URL("@/assets/images/partners/6.webp", import.meta.url).href,
+   title: "مايكروسوفت",
+    image:"https://w7.pngwing.com/pngs/719/781/png-transparent-windows-logo-microsoft-windows-scalable-graphics-logo-computer-file-microsoft-logo-icon-angle-text-rectangle.png",
   },
   {
     id: 7,
-    title: "تيسلا",
-    image: new URL("@/assets/images/partners/3.webp", import.meta.url).href,
+    title: "ادوبي xd",
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUZK5gi1clbPagXVx8PLkTK8qhlBCjve9oRw&s",
   },
   {
     id: 8,
-    title: "مايكروسوفت",
-    image: new URL("@/assets/images/partners/2.webp", import.meta.url).href,
+   title: "مايكروسوفت",
+    image:"https://w7.pngwing.com/pngs/719/781/png-transparent-windows-logo-microsoft-windows-scalable-graphics-logo-computer-file-microsoft-logo-icon-angle-text-rectangle.png",
   },
   {
     id: 9,
-    title: "مايكروسوفت",
-    image: new URL("@/assets/images/partners/2.webp", import.meta.url).href,
+    title: "ادوبي xd",
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUZK5gi1clbPagXVx8PLkTK8qhlBCjve9oRw&s",
   },
   {
     id: 10,
-    title: "تيسلا",
-    image: new URL("@/assets/images/partners/3.webp", import.meta.url).href,
+    title: "ادوبي xd",
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUZK5gi1clbPagXVx8PLkTK8qhlBCjve9oRw&s",
   },
-  {
-    id: 11,
-    title: "مايكروسوفت",
-    image: new URL("@/assets/images/partners/2.webp", import.meta.url).href,
-  },
-  {
-    id: 12,
-    title: "تيسلا",
-    image: new URL("@/assets/images/partners/3.webp", import.meta.url).href,
-  },
-];
+]
+
+// const items = [
+//   {
+//     id: 1,
+//     title: "ميرو",
+//     image: new URL("@/assets/images/partners/1.webp", import.meta.url).href,
+//   },
+//   {
+//     id: 2,
+//     title: "مايكروسوفت",
+//     image: new URL("@/assets/images/partners/2.webp", import.meta.url).href,
+//   },
+//   {
+//     id: 3,
+//     title: "تيسلا",
+//     image: new URL("@/assets/images/partners/3.webp", import.meta.url).href,
+//   },
+//   {
+//     id: 4,
+//     title: "أوتو",
+//     image: new URL("@/assets/images/partners/4.webp", import.meta.url).href,
+//   },
+//   {
+//     id: 5,
+//     title: "ادوبي xd",
+//     image: new URL("@/assets/images/partners/5.webp", import.meta.url).href,
+//   },
+//   {
+//     id: 6,
+//     title: "ميرو",
+//     image: new URL("@/assets/images/partners/6.webp", import.meta.url).href,
+//   },
+//   {
+//     id: 7,
+//     title: "تيسلا",
+//     image: new URL("@/assets/images/partners/3.webp", import.meta.url).href,
+//   },
+//   {
+//     id: 8,
+//     title: "مايكروسوفت",
+//     image: new URL("@/assets/images/partners/2.webp", import.meta.url).href,
+//   },
+//   {
+//     id: 9,
+//     title: "مايكروسوفت",
+//     image: new URL("@/assets/images/partners/2.webp", import.meta.url).href,
+//   },
+//   {
+//     id: 10,
+//     title: "تيسلا",
+//     image: new URL("@/assets/images/partners/3.webp", import.meta.url).href,
+//   },
+//   {
+//     id: 11,
+//     title: "مايكروسوفت",
+//     image: new URL("@/assets/images/partners/2.webp", import.meta.url).href,
+//   },
+//   {
+//     id: 12,
+//     title: "تيسلا",
+//     image: new URL("@/assets/images/partners/3.webp", import.meta.url).href,
+//   },
+// ];
 
 const swiperOptions = {
   autoPlay: true,
