d3e09634737e774097a5c88aa4c7bf4b9745d4a3
mahmoud-ramadn <ahmedshawareb567@gmail.com>
2025-03-11 23:28:13 +0200

    change:swiper-buttons-place in OurServices-Section

--- components/OurServices.vue
+++ components/OurServices.vue
@@ -1,9 +1,39 @@
 <template>
   <div
     id="our-services"
-    class="pt-6 relative bg-gradient-to-tr from-purple-500 to-purple-900 lg:pb-28  pb-10"
+    class="pt-6  relative bg-gradient-to-tr from-purple-500 to-purple-900 lg:pb-28  pb-10"
   >
+  
     <UiTitle title="خدماتنا" class="mb-8 text-white before:bg-white" />
+ <div
+      class=" flex  gap-x-5  lg:bottom-[7rem]  lg:left-[60%] left-1/2 -translate-x-1/2  md:bottom-[5rem]  bottom-[4rem]  absolute z-50  "
+    >
+      <button
+        @click="goToPrev"
+        :class="{
+          'bg-white text-purple-500': activeButton === 'prev',
+          'bg-transparent text-white': activeButton !== 'prev',
+        }"
+        class="border-2 border-white  size-10 flex justify-center items-center rounded-full disabled:cursor-not-allowed hover:bg-purple-700 transition-colors"
+      >
+        <svg-icon name="arrow-right" class="text-current " />
+      </button>
+
+      <button
+        @click="goToNext"
+        :class="{
+          'bg-white text-purple-500': activeButton === 'next',
+          'bg-transparent  text-purple-500': activeButton !== 'next',
+        }"
+        class="border-2 border-white bg-white text-purple-500   size-10 flex justify-center items-center rounded-full disabled:cursor-not-allowed hover:bg-purple-700 transition-colors"
+      >
+        <svg-icon name="arrow-right" class="text-current rotate-180" />
+      </button>
+    </div>
+   
+
+
+
     <swiper-wrapper
       ref="swiperRef"
       class="px-0 mx-0"
@@ -41,31 +71,7 @@
         </div>
       </template>
     </swiper-wrapper>
-    <div
-      class=" md:container w-full flex md:justify-start justify-center  md:absolute      bottom-28  md:left-1/2 md:-translate-x-1/2  items-start space-x-4 rtl:space-x-reverse"
-    >
-      <button
-        @click="goToPrev"
-        :class="{
-          'bg-white text-purple-500': activeButton === 'prev',
-          'bg-transparent text-white': activeButton !== 'prev',
-        }"
-        class="border-2 border-white  size-10 flex justify-center items-center rounded-full disabled:cursor-not-allowed hover:bg-purple-700 transition-colors"
-      >
-        <svg-icon name="arrow-right" class="text-current " />
-      </button>
-
-      <button
-        @click="goToNext"
-        :class="{
-          'bg-white text-purple-500': activeButton === 'next',
-          'bg-transparent  text-purple-500': activeButton !== 'next',
-        }"
-        class="border-2 border-white bg-white text-purple-500 size-10 flex justify-center items-center rounded-full disabled:cursor-not-allowed hover:bg-purple-700 transition-colors"
-      >
-        <svg-icon name="arrow-right" class="text-current rotate-180" />
-      </button>
-    </div>
+    
   </div>
 </template>
 
